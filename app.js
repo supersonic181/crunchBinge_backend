ee4fc12bc2bb0cf0134bb32dc80e149265cff04e
Shubham Shaw <shubhamshaw022@gmail.com>
2022-12-28 01:28:17 +0530

    port updated

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.cors
+}));
 appService.connectToDB();
 appService.setAPIMiddleware(app);
 appService.apiSetUp(app);
